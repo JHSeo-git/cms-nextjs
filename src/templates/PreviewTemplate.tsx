9c825a9d02d1e0f78c4e5c09dd028deea41709b6
JHSeo <qnfqnfqnf@gmail.com>
2021-01-05 22:27:57 +0900

    debug

--- src/templates/PreviewTemplate.tsx
+++ src/templates/PreviewTemplate.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import * as Core from 'netlify-cms-core';
+import styled from 'styled-components';
 import HighlightWrapper from '../components/mdx/HighlightWrapper';
 
+const Title = styled.h1`
+  text-align: center;
+  border-bottom: ${(props) => props.theme.GrayColor.Color100};
+`;
+
 const PreviewTemplate = (props: Core.PreviewTemplateComponentProps) => {
-  const { entry } = props;
-  const body = entry.getIn(['data', 'body']);
-  console.log('entry', entry);
-  return <HighlightWrapper>{body}</HighlightWrapper>;
+  const { entry, widgetFor } = props;
+  const title = entry.getIn(['data', 'title']);
+  const body = widgetFor('body');
+  console.log('entry', title, body);
+  return (
+    <>
+      <Title>{title}</Title>
+      <HighlightWrapper>{body}</HighlightWrapper>
+    </>
+  );
 };
 
 export default PreviewTemplate;
