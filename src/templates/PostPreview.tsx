d38475acf59c000cc57707f7f047d2b66554912a
JHSeo <qnfqnfqnf@gmail.com>
2021-01-07 19:09:52 +0900

    cms hljs

--- src/templates/PostPreview.tsx
+++ src/templates/PostPreview.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { PreviewTemplateComponentProps } from 'netlify-cms-core';
 import styled from 'styled-components';
 import renderToString from 'next-mdx-remote/render-to-string';
